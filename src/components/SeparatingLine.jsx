0288c552bb415f8e66518add503a605d57ba4365
BijetaSah <bijetasah@30gmail.com>
2025-10-28 16:30:02 +0530

    task: made a box looks like line for separation purpose

---
+++ src/components/SeparatingLine.jsx
@@ -0,0 +1,7 @@
+function SeparatingLine() {
+  return (
+    <div class="w-[620px] h-1 bg-[#282828] shadow-[ shadow-[4px_4px_4.9px_0_rgba(0,0,0,0.33)] rounded-lg my-3"></div>
+  );
+}
+
+export default SeparatingLine;
